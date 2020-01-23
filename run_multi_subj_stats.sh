90c96b8c974f9055bb61ec1b70fc93b2878996b8
CMilbourn <60184621+CMilbourn@users.noreply.github.com>
2020-01-23 15:19:14 +0000

    run_multi_subj_stats.sh
    Runs through a loop of skip numbers 000 in steps of 4seconds to 120. 
    creates cvrmaps
    creates zstat2 and merges all of the zstat2's into a concat image

--- run_multi_subj_stats.sh
+++ run_multi_subj_stats.sh
@@ -6,7 +6,7 @@ codedir=`dirname $0` #assume that other scripts are in the same directory
 
 #Print out usage of the code and input parameters
 if [ $# -lt 1 ]; then
-  echo "Usage: $0 <sourcedata folder> <subjectnumber> e.g. ./run_one_subj_stats_test.sh /Users/colette/sourcedata sub-04"
+  echo "Usage: $0 <sourcedata folder> <subjectnumber> <MRIParam> e.g. ./run_multi_subj_stats.sh /Users/colette/sourcedata sub-02 paramA"
 
 fi
 
@@ -15,33 +15,29 @@ fi
 src=$1 
 echo "Sourcedata Folder:" ${1}
 subj=$2 #Input 2 is the subject ID in the format sub-01
+MRIParam=$3 #Input 3 is the MRI parameter default, paramA or paramB
 echo "Subject selected:" ${2}
 echo "Number of arguments passed:" $# 
-#MRIparam=$3
-#cd ${src}
 
 
 #echo $PWD
-	
+#Loop through skip number 000 in steps of 4 seconds through to 120, n=Skip Number	
 for n in $(seq -w 0 4 120);
 do
 	echo $n
-	${codedir}/one_subj_stats.sh ${src} ${subj} default ${n}	
-	#${codedir}/one_subj_stats.sh ${src} ${subj} ${MRIparam} ${n}
+	${codedir}/multi_subj_stats.sh ${src} ${subj} ${MRIParam} ${n}
 done
  
-srcout=${src}/derivatives/StatsOutput/${subj}
-cd ${srcout}/Zstat2_${subj}
-echo "Changing to folder:" ${srcout}/Zstat2_${subj}
+srcout=${src}/derivatives/StatsOutput/${subj}/${subj}_${MRIParam}
+srcout=${src}/derivatives/StatsOutput/${subj}/${subj}_${MRIParam}
+cd ${srcout}/Zstat2_${subj}_${MRIParam}
+echo "Changing to folder:" ${srcout}/Zstat2_${subj}_${MRIParam}
 chmod u+w ./*
 #echo "current directory is:"${PWD};
 
 echo "Merging zstat2 Files..."
-if ! [ -d "${srcout}/Zstat2_${subj}" ]; then #check if the output directory exists, if not make it
-echo "File" ${srcout}/Zstat2_${subj} "NOT FOUND!"
+if ! [ -d "${srcout}/Zstat2_${subj}_${MRIParam}" ]; then #check if the output directory exists, if not make it
+echo "File" ${srcout}/Zstat2_${subj}_${MRIParam} "NOT FOUND!"
 fi
-fslmerge -t zstat2_concat_${subj} $zstat2_${subj}*    
-
- 
-
+fslmerge -t ${srcout}/Zstat2_${subj}_${MRIParam}/zstat2_concat_${subj}_${MRIParam} ${srcout}/Zstat2_${subj}_${MRIParam}/zstat2_${subj}*    
 
